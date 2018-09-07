701fc83fd566b774a2ba03cfa9ede883d4425e68
Ian Maleney <imaleney@gmail.com>
2018-09-07 17:14:14 +0100

    Testing Pagination Toggle

--- template-parts/archive/content-archive__list.php
+++ template-parts/archive/content-archive__list.php
@@ -4,10 +4,16 @@
 			while ( have_posts() ) : the_post();
 			get_template_part( 'template-parts/content', 'archive__module' );
 			endwhile; ?>
+
+			<?php 
+			$paged = get_query_var('paged');
+			echo $paged;
+			if ( $paged ) : ?>
 			<div class="pagination__wrapper">
 				<div class="page-nav nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
 				<div class="page-nav nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
 			</div>
+		<?php endif; ?>
 		<?php } else {
 			?><div class="search-error-message"><h2>Sorry, we could not find any posts that matched your query.</h2></div><?php
 		}
