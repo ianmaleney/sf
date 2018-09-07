bff2b129e01fd8b948ea39ae6d0b5c78f2fefad8
Ian Maleney <imaleney@gmail.com>
2018-09-07 16:50:40 +0100

    Review Header Fixes

--- template-parts/content-review.php
+++ template-parts/content-review.php
@@ -14,19 +14,6 @@
 					<?php } else { ?>
 						<img src="<?php the_post_thumbnail_url( 'large' ); ?>" />
 					<?php } ?>
-				<ul class="c-book-review__info-list">
-					<li class="c-book-review__info-list-item"><span class="c-list-item--title">Title:</span> <?php the_title(); ?></li>
-					<li class="c-book-review__info-list-item"><span class="c-list-item--title">Author:</span> <?php the_field("book_author"); ?></li>
-					<li class="c-book-review__info-list-item"><span class="c-list-item--title">Publisher:</span> <?php the_field("book_publisher"); ?></li>
-					<li class="c-book-review__info-list-item"><span class="c-list-item--title">Published:</span>
-						<?php
-							$date = get_field("date_published");
-							$date = new DateTime($date);
-							echo $date->format('M Y');
-						?>
-					</li>
-					<li class="c-book-review__info-list-item"><span class="c-list-item--title">Price:</span> <?php the_field("price"); ?></li>
-				</ul>
 			</div>
 			<div class="c-article__info c-article__info--review">
 				<h3 class="heading-3 c-article__info--date">
@@ -38,14 +25,24 @@
 						}
 					 ?><?php sf_single_cat(); ?>
 				</h3>
-				<h1 class="heading-1 c-article__info--title c-article__info--title--review">
+				<h1 class="c-article__info--title c-article__info--title--review">
 					<?php the_title(); ?>
 				</h1>
-				<h2 class="heading-4 c-book-review__author"><i>by</i> <?php the_field("book_author"); ?></h2>
-				<h2 class="heading-3 c-article__info--author">
+				<p class="c-book-review__author"><i>by</i> <?php the_field("book_author"); ?></h2>
+				<p class="c-article__info--author">
 					<span class="c-author-link c-author-link--unlinked">Reviewed by&nbsp;</span><?php guest_author_link(); ?>
 				</h2>
-				
+				<ul class="c-book-review__info-list">
+					<li class="c-book-review__info-list-item"><span class="c-list-item--title">Publisher:</span> <?php the_field("book_publisher"); ?></li>
+					<li class="c-book-review__info-list-item"><span class="c-list-item--title">Published:</span>
+						<?php
+							$date = get_field("date_published");
+							$date = new DateTime($date);
+							echo $date->format('M Y');
+						?>
+					</li>
+					<li class="c-book-review__info-list-item"><span class="c-list-item--title">Price:</span> <?php the_field("price"); ?></li>
+				</ul>
 			</div>
 		</section>
 		<div class="js-article__social-icons c-article__social-icons">
