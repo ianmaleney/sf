1d689b5ee53c94b0ad79b273ad8b07f30a5ddb39
Ian Maleney <imaleney@gmail.com>
2019-03-21 13:48:09 +0000

    Header Ad lazy-load & link='preload'

--- header.php
+++ header.php
@@ -9,8 +9,8 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
-	<link href="https://fonts.googleapis.com/css?family=Amiri:400,400i,700" rel="stylesheet">
-	<link rel="stylesheet" href="https://use.typekit.net/zsp1myo.css">
+	<link href="https://fonts.googleapis.com/css?family=Amiri:400,400i,700" rel="preload" as="style">
+	<link as="style" rel="preload" href="https://use.typekit.net/zsp1myo.css">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,7 +28,7 @@
 		<?php } else { ?>
 				<div class="c-primary-header__sub-ad">
 					<a href="/shop">
-						<img src="/wp-content/themes/stingingfly/img/sf-header-ad-v1.jpg" alt="Stinging Fly Sale!">
+						<img data-src="/wp-content/themes/stingingfly/img/sf-header-ad-v1.jpg" alt="Stinging Fly Sale!">
 					</a>
 				</div>
 		<?php } ?>
