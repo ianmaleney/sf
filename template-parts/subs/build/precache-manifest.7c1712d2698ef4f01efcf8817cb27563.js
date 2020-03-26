df3239b87165b350a8eaaf9010e0ba808755f693
Ian Maleney <imaleney@gmail.com>
2020-03-26 20:32:48 +0000

    New Subs Dash

---
+++ template-parts/subs/build/precache-manifest.7c1712d2698ef4f01efcf8817cb27563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1defa925902a76068da0bfb0f72fd573",
+    "url": "/wp-content/themes/stingingfly/template-parts/subs/build/index.html"
+  },
+  {
+    "revision": "ac04d4d61ff7d77b7269",
+    "url": "/wp-content/themes/stingingfly/template-parts/subs/build/static/css/2.c3f1451c.chunk.css"
+  },
+  {
+    "revision": "4c4d252f108b53b461eb",
+    "url": "/wp-content/themes/stingingfly/template-parts/subs/build/static/css/main.441ceb7c.chunk.css"
+  },
+  {
+    "revision": "ac04d4d61ff7d77b7269",
+    "url": "/wp-content/themes/stingingfly/template-parts/subs/build/static/js/2.0da3ab5e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wp-content/themes/stingingfly/template-parts/subs/build/static/js/2.0da3ab5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c4d252f108b53b461eb",
+    "url": "/wp-content/themes/stingingfly/template-parts/subs/build/static/js/main.419bb8a4.chunk.js"
+  },
+  {
+    "revision": "06cbe2730a58f9f05ea2",
+    "url": "/wp-content/themes/stingingfly/template-parts/subs/build/static/js/runtime-main.a314bb34.js"
+  }
+]);
