7d33bdffd6202c1b5183b1d42c26e9162d51dae6
Ian Maleney <imaleney@gmail.com>
2020-06-26 10:54:47 +0100

    Featured Content

--- template-parts/home-page/sf-featured-content.php
+++ template-parts/home-page/sf-featured-content.php
@@ -75,7 +75,7 @@ if($secondaryPosts) {
         <?php if( get_field('issue_volume') || get_field('isbn')) { ?>
           <!-- Do Nothing -->
         <?php } else { ?>
-         <?php if ('page' != get_post_type()) { ?>
+         <?php if ('page' != get_post_type() && 'product' != get_post_type()) { ?>
             <p class="c-content-type"><?php sf_single_cat() ?></p>
             <p class="sep">|</p>
           <?php } ?>
