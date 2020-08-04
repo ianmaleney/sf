ef119e2bbbbcf42bb44c0e599c4b85d9c4136191
Ian Maleney <imaleney@gmail.com>
2020-08-04 20:20:36 +0100

    Finished author-box block

--- blocks/author-box/block.php
+++ blocks/author-box/block.php
@@ -4,9 +4,7 @@
 	</div>
 	<div class="author-text-wrapper">
 		<p class="author-box__bio"><span class="author-box__name"><?php block_field( 'author-name' ); ?></span> <?php block_field( 'author-bio' ); ?></p>
-		<p class="author-box__note"><span class="author-box-about">About <?php block_field( 'piece-title' ); ?>:</span> <?php block_field( 'author-note' ); ?></p>
+		<p class="author-box__note"><span class="author-box__about">About <em><?php block_field( 'piece-title' ); ?></em>:</span> <?php block_field( 'author-note' ); ?></p>
 	</div>
-	<div class="end">
-		&#9632;&#9632;&#9632;
-	</div>
+	<div class="end">&#9632;&#9632;&#9632;</div>
 </div>
