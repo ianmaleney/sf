a1526323fe721548d8b9759e1b48ae784983821e
Ian Maleney <imaleney@gmail.com>
2020-08-04 18:08:30 +0100

    Keep styling that box

--- blocks/author-box/block.php
+++ blocks/author-box/block.php
@@ -1,5 +1,9 @@
 <div class="author-box">
-	<h2 class="author-box__name"><?php block_field( 'author-name' ); ?></h2>
-	<p class="author-box__bio"><?php block_field( 'author-bio' ); ?></p>
-	<p class="author-box__note"><?php block_field( 'author-note' ); ?></p>
+	<div class="author-image-wrapper">
+		<img src="<?php block_field( 'author-image' ); ?>" alt="<?php block_field( 'author-name' ); ?>" class="author-image">
+	</div>
+	<div class="author-text-wrapper">
+		<p class="author-box__bio"><span class="author-box__name"><?php block_field( 'author-name' ); ?></span> <?php block_field( 'author-bio' ); ?></p>
+		<p class="author-box__note"><span class="author-box-about">About <?php block_field( 'piece-title' ); ?>:</span> <?php block_field( 'author-note' ); ?></p>
+	</div>
 </div>
