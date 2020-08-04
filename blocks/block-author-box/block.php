ff3a142fe3067049a227fc9a20ec2f3d6da121aa
Ian Maleney <imaleney@gmail.com>
2020-08-04 17:16:22 +0100

    Testing a custom block

---
+++ blocks/block-author-box/block.php
@@ -0,0 +1,5 @@
+<div class="author-box">
+	<h2 class="author-box__name"><?php block_field( 'author-name' ); ?></h2>
+	<p class="author-box__bio"><?php block_field( 'author-bio' ); ?></p>
+	<p class="author-box__note"><?php block_field( 'author-note' ); ?></p>
+</div>
