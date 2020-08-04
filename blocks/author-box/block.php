8dc7de69ec2a4b74bcfc197535c767e143562770
Ian Maleney <imaleney@gmail.com>
2020-08-04 19:34:34 +0100

    Block block baby

--- blocks/author-box/block.php
+++ blocks/author-box/block.php
@@ -6,4 +6,7 @@
 		<p class="author-box__bio"><span class="author-box__name"><?php block_field( 'author-name' ); ?></span> <?php block_field( 'author-bio' ); ?></p>
 		<p class="author-box__note"><span class="author-box-about">About <?php block_field( 'piece-title' ); ?>:</span> <?php block_field( 'author-note' ); ?></p>
 	</div>
+	<div class="end">
+		&#9632;&#9632;&#9632;
+	</div>
 </div>
